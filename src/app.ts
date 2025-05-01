9bcec630b04395adfd7ad535c1ebbb4a7345802e
David Urbano <60886336+sanurb@users.noreply.github.com>
2025-05-01 13:27:06 -0500

    feat: add typia and agentica
    changes:
    - replace swc for tsc
    - add envs config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import responseTime from "response-time";
 import { type Express, json, urlencoded } from "ultimate-express";
 import express from "ultimate-express";
+import { SGlobal } from "./SGlobal";
 import { mountRoutes } from "./routes";
 
 export const app: Express = express();
+if (SGlobal.env.OPENAI_API_KEY === undefined)
+  console.error("env.OPENAI_API_KEY is not defined.");
 
 app.set("catch async errors", true);
-app.set("port", process.env.PORT ?? 3000);
+app.set("port", SGlobal.env.PORT ?? 3000);
 
 app.use(responseTime());
 app.use(json({ limit: "100mb" }));
