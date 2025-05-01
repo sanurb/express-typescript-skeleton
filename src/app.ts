756743951912935412568cc0191231c51e3fbc66
Abhishek Keshri <60886336+sanurb@users.noreply.github.com>
2025-05-01 12:15:11 -0500

    fix: import app in server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import compression from "compression";
 import responseTime from "response-time";
 import { type Express, json, urlencoded } from "ultimate-express";
 import express from "ultimate-express";
@@ -11,7 +10,6 @@ app.set("port", process.env.PORT ?? 3000);
 
 app.use(responseTime());
 app.use(json({ limit: "100mb" }));
-app.use(compression({ threshold: 1 }));
 app.use(urlencoded({ extended: true }));
 
 // only 1-level deep routers can be optimized
